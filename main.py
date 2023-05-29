2006ca017e8e1d4764ef5fdaf57d2b4366280fa3
arch linuxer <109875196+caernarferon@users.noreply.github.com>
2023-05-29 11:44:33 -0600

    it actually works now, fixed creating directories

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import nextcord
 from nextcord.ext import commands
 import logging
 from dotenv import load_dotenv
+import asyncio
+
 load_dotenv()
 logging.basicConfig(level=logging.WARNING)
 
@@ -15,31 +17,36 @@ handler = logging.FileHandler(filename='nextcord.log', encoding='utf-8', mode='w
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+home_dir = os.path.expanduser("~")
+parliament_dir = os.path.join(home_dir, ".parliamentbomber")
+os.makedirs(parliament_dir, exist_ok=True)
+
+
 @bot.event
 async def on_ready():
     print(f"Ready, using {bot.user}")
 
-async def log_message(message, guild_id):
-    home_dir = os.path.expanduser("~")
-    parliament_dir = os.path.join(home_dir, ".parliamentbomber")
-    os.makedirs(parliament_dir, exist_ok=True)
 
+async def log_message(message, guild_id):
     guild_dir = os.path.join(parliament_dir, f'guild_{guild_id}')
     os.makedirs(guild_dir, exist_ok=True)
 
     channel_name = message.channel.name.replace(' ', '_').lower()
     filename = os.path.join(guild_dir, f'{channel_name}.txt')
+
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(f'{message.author.name}: {message.content}\n')
 
+
 async def log_all_past_messages():
-    tasks = []
     for guild in bot.guilds:
-        for channel in guild.channels:
-            if isinstance(channel, nextcord.TextChannel):
-                async for message in channel.history(limit=None):
-                    task = log_message(message, guild.id)
-                    tasks.append(task)
+        for channel in guild.text_channels:
+            guild_dir = os.path.join(parliament_dir, f'guild_{guild.id}')
+            os.makedirs(guild_dir, exist_ok=True)
+
+            async for message in channel.history(limit=None):
+                await log_message(message, guild.id)
+
 
 @bot.event
 async def on_message(message):
@@ -49,9 +56,17 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-@bot.slash_command(description="My first slash command")
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Hello!")
+
+@bot.slash_command(description="Log all past messages")
+async def log_past_messages(interaction: nextcord.Interaction):
+    await interaction.send("Logging all past messages...")
     await log_all_past_messages()
-token = os.environ.get("TOKEN") 
-bot.run(token) 
+
+
+@bot.slash_command(description="Initialize logger")
+async def init_logger(interaction: nextcord.Interaction):
+    await interaction.send("Logger initialized.")
+
+
+token = os.environ.get("TOKEN")
+bot.run(token)
