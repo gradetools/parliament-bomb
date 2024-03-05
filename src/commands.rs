a355bccfea4cacd0a84a6e7144d7b71de2d0bf3f
spacefault <74156492+spacefault@users.noreply.github.com>
2024-03-05 13:08:30 -0700

    a bunch of changes
    migrate to poise

---
+++ src/commands.rs
@@ -0,0 +1,83 @@
+use crate::{Context, Error};
+
+// show this help menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "These are the available commands in Parliamentbomb.",
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+// vote for something
+// type in "/vote pumpkin" to vote for pumpkins
+// TODO: lock voting options to admins only
+#[poise::command(prefix_command, slash_command)]
+pub async fn vote(
+    ctx: Context<'_>,
+    #[description = "What to vote for"] choice: String,
+) -> Result<(), Error> {
+    // Lock the Mutex in a block {} so the Mutex isn't locked across an await point
+    let num_votes = {
+        let mut hash_map = ctx.data().votes.lock().unwrap();
+        let num_votes = hash_map.entry(choice.clone()).or_default();
+        *num_votes += 1;
+        *num_votes
+    };
+
+    let response = format!("Successfully voted for {choice}. {choice} now has {num_votes} votes!");
+    ctx.say(response).await?;
+    Ok(())
+}
+
+// get vote count
+//
+// get the number of votes either in general, or for a specific choice:
+// "/getvotes"
+// "/getvotes pumpkin"
+#[poise::command(prefix_command, track_edits, aliases("votes"), slash_command)]
+pub async fn getvotes(
+    ctx: Context<'_>,
+    #[description = "Choice to retrieve votes for"] choice: Option<String>,
+) -> Result<(), Error> {
+    if let Some(choice) = choice {
+        let num_votes = *ctx.data().votes.lock().unwrap().get(&choice).unwrap_or(&0);
+        let response = match num_votes {
+            0 => format!("Nobody has voted for {} yet", choice),
+            _ => format!("{} people have voted for {}", num_votes, choice),
+        };
+        ctx.say(response).await?;
+    } else {
+        let mut response = String::new();
+        for (choice, num_votes) in ctx.data().votes.lock().unwrap().iter() {
+            response += &format!("{}: {} votes", choice, num_votes);
+        }
+
+        if response.is_empty() {
+            response += "Nobody has voted for anything yet :(";
+        }
+
+        ctx.say(response).await?;
+    };
+
+    Ok(())
+}
+
+// about command
+// run this command with "/about"
+#[poise::command(slash_command, prefix_command)]
+pub async fn reply(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.reply(format!("Hello {}! You are using the latest ParliamentBomb Rust Binary.", ctx.author().name)).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,138 @@
+#![warn(clippy::str_to_string)]
+
 mod commands;
 
+use poise::serenity_prelude as serenity;
+use colored::Colorize;
 use dotenv::dotenv;
 use std::env;
-use serenity::async_trait;
-use serenity::builder::{CreateInteractionResponse, CreateInteractionResponseMessage};
-use serenity::client::{Client, EventHandler};
-use serenity::model::application::{Command, Interaction};
-use serenity::model::gateway::Ready;
-use serenity::model::id::GuildId;
-use serenity::prelude::*;
+use std::path::PathBuf;
+use ctrlc;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
-struct Handler;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::Command(command) = interaction {
-            println!("Received command interaction: {:#?}", command);
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-            let content = match command.data.name.as_str() {
-                "ping" => Some(commands::ping::run(&command.data.options())),
-                _ => Some("not implemented :(".to_string()),
-            };
+fn get_binary_location() -> String {
+    let binary_location: PathBuf = match env::current_exe() {
+        Ok(path) => path,
+        Err(e) => {
+            println!("Failed to get the binary location: {}", e);
+            return "Unknown location".to_string(); 
+        }
+    };
 
-            if let Some(content) = content {
-                let data = CreateInteractionResponseMessage::new().content(content);
-                let builder = CreateInteractionResponse::Message(data);
-                if let Err(why) = command.create_response(&ctx.http, builder).await {
-                    println!("Cannot respond to slash command: {:?}", why);
-                }
+    // Convert PathBuf to String
+    binary_location.to_str().unwrap_or("Unknown location").to_string()
+}
+
+pub struct Data {
+    votes: Mutex<HashMap<String, u32>>,
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
             }
         }
-    }
-
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-
-        let guild_id = GuildId::new(
-            env::var("GUILD_ID")
-                .expect("Expected GUILD_ID in environment")
-                .parse()
-                .expect("GUILD_ID must be an integer"),
-        );
-
-        let commands = guild_id
-            .set_commands(&ctx.http, vec![
-                commands::ping::register(),
-            ])
-            .await;
     }
 }
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok(); // Load environment variables from .env file
+    env_logger::init();
+    dotenv().ok();
 
-    // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::help(), commands::vote(), commands::getvotes()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("~".into()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            additional_prefixes: vec![
+                poise::Prefix::Literal("hey bot"),
+                poise::Prefix::Literal("hey bot,"),
+            ],
+            ..Default::default()
+        },
+        on_error: |error| Box::pin(on_error(error)),
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
 
-    // Build our client.
-    let mut client = Client::builder(token, GatewayIntents::empty())
-        .event_handler(Handler)
-        .await
-        .expect("Error creating client");
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                let version = env!("CARGO_PKG_VERSION");
+                let binary_location_str = get_binary_location();
+                println!("{}", "Welcome to ParliamentBomb!".green().bold());
+                println!("{}", format!("Version: {}", version));
+                println!("Binary Location: {}", binary_location_str);
+                println!("Successfully Logged in as {}", _ready.user.name);
+                println!(">>>>> BEGIN PARLIAMENTBOMB LOGGING HERE <<<<<");
+                ctrlc::set_handler(move || {
+                    println!(" <-- Ctrl+C pressed, exiting...");
+                    println!(">>>>> END PARLIAMENTBOMB LOGGING HERE <<<<<");
+                    std::process::exit(0); 
+                }).expect("Error setting Ctrl-C handler");
+            
+            
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    votes: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .build();
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Missing `DISCORD_TOKEN` env var, see README for more information.");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap();
 }
-
